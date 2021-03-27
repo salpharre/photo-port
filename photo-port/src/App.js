8a4a28f96210239c0d9c665154eeefda4c535245
Sandra <sandra.alpha2010@gmail.com>
2021-03-26 22:31:21 -0700

    added a new gallery component

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 // import './App.css';
 import About from "./components/About";
 import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div>
       <Nav />
       <main>
+        <Gallery />
         <About />
       </main>
   </div>
