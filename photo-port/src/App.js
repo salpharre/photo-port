8602333ae6ac851bfae1d319f8fdd782a0708304
Sandra <sandra.alpha2010@gmail.com>
2021-03-27 01:35:50 -0700

    added photolist component under gallery component, made it so photos changed based on what category of picture was taken. renders correctly.

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -31,7 +31,9 @@ function App() {
         currentCategory={currentCategory}
       />
       <main>
-        <Gallery />
+        <Gallery 
+          currentCategory={currentCategory}
+        />
         <About />
       </main>
   </div>
