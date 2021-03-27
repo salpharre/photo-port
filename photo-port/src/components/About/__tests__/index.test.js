e37f3dc87aa528eec0b420b09350a25ade5ee555
Sandra <sandra.alpha2010@gmail.com>
2021-03-26 22:02:07 -0700

    added test cases for two components

--- photo-port/src/components/About/__tests__/index.test.js
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import About from "../index";
+import About from "..";
 //global function from Jest
 afterEach(cleanup);
 //declares component we're testing
