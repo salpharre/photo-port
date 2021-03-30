b493780fa163c550ca1aaf0cc556ac7584f4945e
Sandra <sandra.alpha2010@gmail.com>
2021-03-29 17:45:18 -0700

    state grabs and submits values in form correctly

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+    //state for form values
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+    //deconstruct state object so can eliminate 'formState.name' in defaultValue attribute
+    //and use deconstructed properties instead
+    const { name, email, message } = formState;
+    //will set the values of the form to the state using the setter
+    //
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value});
+    }
+    console.log(formState);
+    //submits values in state
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input 
+                    type="text" 
+                    name="name" 
+                    defaultValue={name}
+                    onChange={handleChange}
+                    />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input 
+                    type="email" 
+                    name="email" 
+                    defaultValue={email}
+                    onChange={handleChange}
+                    />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea 
+                    name="message" 
+                    rows="5" 
+                    defaultValue={message}
+                    onChange={handleChange}
+                    />
                 </div>
                 <button type="submit">Submit</button>
             </form>
